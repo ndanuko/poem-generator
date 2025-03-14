c3496d6bfab0db3335f629096a13f5c5847e082d
stella njeri ndanuko <sndanuko@gmail.com>
2025-03-14 22:36:15 +0300

    added javascript and typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-
+function generateLyric(event) {
+  event.preventDefault();
+  new Typewriter("#lyrics", {
+    strings: "I wanna get lost in you",
+    autoStart: true,
+    delay: 1,
+  });
+}
+let lyricFormElement = document.querySelector("#lyrics-generator-form");
+lyricFormElement.addEventListener("submit", generateLyric);
